1612900da6d59b91eb5925bf8915b7f40f82079b
unknown <crystalbritish1120@gmail.com>
2024-04-03 06:56:45 +0900

    0403

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -2,15 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\WebHookEvent;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Laravel\Cashier\Events\WebhookReceived;
 
 class WebHookController extends Controller
 {
     public function onetimeHook(Request $request)
     {
-        $payload = json_decode($request->getContent(), true);
-        WebhookReceived::dispatch($payload);
+        try {
+            Log::info('-----captured----');
+            $payload = json_decode($request->getContent(), true);
+            WebhookReceived::dispatch($payload);
+            return response('Webhook sucess', 200);
+        } catch (Exception $e) {
+            Log::error('webhook error: '. $e->getMessage());
+            return response('Webhook Error', 400);
+        }
     }
 }
